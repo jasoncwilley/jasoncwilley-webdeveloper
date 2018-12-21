ba7a60b4590f383716decc8a342708368412fea3
jasoncwilley <jason.c.willey@gmail.com>
2018-12-20 19:09:20 -0500

    added tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,64 +2,94 @@ jQuery(document).ready(function($) {
 
 
     /*======= Skillset *=======*/
-    
+
     $('.level-bar-inner').css('width', '0');
-    
+
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-        
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
+
         });
 
     });
-    
+
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
-    
-    
+
+
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    
+
     $("#rss-feeds").rss(
-    
+
         //RSS feeds
-        
-        
+
+
         {
         // how many entries do you want?
         // default: 4
         // valid values: any integer
         limit: 3,
-        
+
         // the effect, which is used to let the entries appear
         // default: 'show'
         // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
         effect: 'slideFastSynced',
-        
+
         // outer template for the html transformation
         // default: "<ul>{entries}</ul>"
         // valid values: any string
         layoutTemplate: "<div class='item'>{entries}</div>",
-        
+
         // inner template for each entry
         // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
         // valid values: any string
         entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-        
+
         }
     );
-    
+
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     GitHubCalendar("#github-graph", "IonicaBizau");
-    
-    
+
+
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "jasoncwilley", selector: "#ghfeed" });
 
 
 });
+
+// Dropdown Menu
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu'),
+			arrow = button.querySelector('i.icon-arrow');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			arrow.classList.add('open');
+			arrow.classList.remove('close');
+			event.preventDefault();
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			arrow.classList.remove('open');
+			arrow.classList.add('close');
+			event.preventDefault();
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
