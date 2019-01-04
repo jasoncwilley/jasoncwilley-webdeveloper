92f4d591bfa4d399d8128e5d6e19a50bfb7a0ac1
jasoncwilley <jason.c.willey@gmail.com>
2019-01-03 20:24:44 -0500

    fixed tooltip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,8 @@
 
 
-$(document).ready(function($) {
 
 
-    /*======= Skillset *=======*/
 
-    $('.level-bar-inner').css('width', '0');
-
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-
-        });
-
-    });
-
-    /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
 
 
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
@@ -65,33 +45,3 @@ $(document).ready(function($) {
 
 
 });
-
-// Dropdown Menu
-var dropdown = document.querySelectorAll('.dropdown');
-var dropdownArray = Array.prototype.slice.call(dropdown,0);
-dropdownArray.forEach(function(el){
-	var button = el.querySelector('a[data-toggle="dropdown"]'),
-			menu = el.querySelector('.dropdown-menu'),
-			arrow = button.querySelector('i.icon-arrow');
-
-	button.onclick = function(event) {
-		if(!menu.hasClass('show')) {
-			menu.classList.add('show');
-			menu.classList.remove('hide');
-			arrow.classList.add('open');
-			arrow.classList.remove('close');
-			event.preventDefault();
-		}
-		else {
-			menu.classList.remove('show');
-			menu.classList.add('hide');
-			arrow.classList.remove('open');
-			arrow.classList.add('close');
-			event.preventDefault();
-		}
-	};
-})
-
-Element.prototype.hasClass = function(className) {
-    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
-};
